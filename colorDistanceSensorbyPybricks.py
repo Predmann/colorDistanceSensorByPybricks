f31c3f83131b0de8a0e476b386dc39f330a6df15
Predmann <przemyslaw.redmann@gmail.com>
2024-12-20 20:15:58 +0100

    Added comments

--- colorDistanceSensorbyPybricks.py
+++ colorDistanceSensorbyPybricks.py
@@ -1,10 +1,11 @@
 from pybricks.pupdevices import ColorDistanceSensor, DCMotor # ColorSensor instead of ColorDistanceSensor if using Mindstorms sensor
-from pybricks.parameters import Color, Port
+from pybricks.parameters import Color, Port 
 from pybricks.tools import wait
 
-motor = DCMotor(Port.A)
+motor = DCMotor(Port.A) # Motor connected to port A
 sensor = ColorDistanceSensor(Port.B) # ColorSensor instead of ColorDistanceSensor if using Mindstorms sensor
 
+# Constants
 LIGHT = 57
 DARK = 16
 station_stop_time_ms = 5000
@@ -12,11 +13,15 @@ eol_stop_time_ms = 1500
 forward_speed = 50
 check_color_interval_ms = 20
 
+# Function to check for a specific color
 def check_for_color(color):
     while sensor.color() != color:
         wait(check_color_interval_ms)
 
+# Main loop
 while True:
+
+    # Sensor checks for green, will do a pause and move forward
     print("...looking for green...")
     check_for_color(Color.GREEN)
     Kolor = str(sensor.color())
@@ -26,6 +31,7 @@ while True:
     wait(i)
     motor.dc(forward_speed)
 
+    # Sensor checks for blue, will do a pause and move back
     print("...looking for blue...")
     check_for_color(Color.BLUE)
     Kolor = str(sensor.color())
